a9142dc5620b4e1f6b850e800e2508e569d3947a
Jasper Ruoff <jruoff@gmail.com>
2013-02-24 22:11:22 +0100

    Make multi registry setup possible.

--- exercise_1/src/Main.java
+++ exercise_1/src/Main.java
@@ -1,14 +1,132 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.rmi.ConnectException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
 public class Main {
+	private static int id = -1;
+	private static ArrayList<String> slots = new ArrayList<String>();
+	private static ArrayList<ITotalOrdering> friends = new ArrayList<ITotalOrdering>();
 	
-	/**
-	 * @param args
-	 */
+	private static void readConfig() {
+		try {
+			FileInputStream fstream = new FileInputStream("../etc/hosts.conf");
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			
+			String strLine;
+			while ((strLine = br.readLine()) != null) {
+				String[] options = strLine.split(" ");
+				for (int i = 0; i < Integer.parseInt(options[0]); i++)
+					slots.add("rmi://" + options[1] + "/TotalOrdering/" + i);
+			}
+			
+			in.close();
+			fstream.close();
+		} catch (Exception e) {
+			System.out.println("Error reading config file");
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	
+	private static void bind(TotalOrdering instance) throws RemoteException {
+		String uri;
+		
+		for (id = 0; id < slots.size(); id++) {
+			
+			uri = slots.get(id);
+			
+			try {
+				Naming.bind(uri, instance);
+			} catch (Exception e) {
+				continue;
+			}
+			
+			Runtime.getRuntime().addShutdownHook(new Unbinder(uri));
+			
+			System.out.println("BIND: " + id + ": " + uri);
+			System.out.println();
+			return;
+		}
+		
+		System.out.println("Faild to bind!");
+		System.exit(1);
+	}
+	
+	private static void connect() throws InterruptedException {
+		for (int i = 0; i < slots.size(); i++) {
+			String uri = slots.get(i);
+			while (true) {
+				try {
+					ITotalOrdering remote = (ITotalOrdering) Naming.lookup(uri);
+					friends.add(remote);
+					System.out.println("CONNECTED: " + i + ": " + uri);
+					break;
+				} catch (Exception e) {
+					Thread.sleep(500);
+				}
+			}
+		}
+		System.out.println();
+	}
+	
+	private static void broadcast() throws RemoteException {
+		for (int i = 0; i < friends.size(); i++) {
+			ITotalOrdering remote = friends.get(i);
+			try {
+				remote.test("from " + id + ": Hi!");
+			} catch (ConnectException e) {
+				try {
+					remote = (ITotalOrdering) Naming.lookup(slots.get(i));
+					friends.set(i, remote);
+					remote.test("from " + id + ": Hi!");
+				} catch (Exception e2) {
+					System.out.println("Could not connect to remote instance.");
+				}
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		try {
-			System.out.println("Starting LocateRegistry...");
-			java.rmi.registry.LocateRegistry.createRegistry(9090);
+			readConfig();
+			bind(new TotalOrdering());
+			
+			System.out.println("Connecting to friends...");
+			connect();
+			
+			System.out.println("Broadcast...");
+			Thread.sleep(1000);
+			broadcast();
+			
+			Thread.sleep(1000);
 		} catch (Exception e) {
 			e.printStackTrace();
-		}	
+			System.exit(1);
+		}
+		
+		System.out.println("END");			
 	}
+}
+
+class Unbinder extends Thread {
+	public String uri;
+	
+	public Unbinder(String uri) {
+		this.uri = uri;
+	}
+	
+	public void run() {
+		System.out.println("Unbinding uri");
+		try {
+        	Naming.unbind(uri);
+        } catch (Exception e) {
+			Runtime.getRuntime().halt(1);
+		}
+    }
 }
--- exercise_1/src/TotalOrdering.java
+++ exercise_1/src/TotalOrdering.java
@@ -1,122 +1,14 @@
-import java.rmi.*;
+import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
-import java.util.ArrayList;
 
 public class TotalOrdering extends UnicastRemoteObject implements ITotalOrdering {
 	private static final long serialVersionUID = -5840789656649365132L;
-
-	private static ArrayList<ITotalOrdering> friends = new ArrayList<ITotalOrdering>();
 	
 	protected TotalOrdering() throws RemoteException {
 		super();
 	}
 
 	public void test(String oh_hai) throws RemoteException {
-		System.out.println("Received: " + oh_hai);
+		System.out.println("RECV: " + oh_hai);
 	}
-	
-	public static void main(String[] args) {
-		try {
-			String host = "localhost";
-			int port = 9090;
-			int n, id = 0;
-			try {
-				n  = Integer.parseInt(args[0]);
-				
-				if (args.length > 1)
-					host = args[1];
-				
-				if (args.length > 2)
-					port = Integer.parseInt(args[2]);
-				
-			} catch (Exception e) {
-				System.out.println("Usage: program <n> [host=localhost] [port=9090]");
-				System.exit(0);
-				return;
-			}
-			
-			String uri_base = "rmi://" + host + ":" + port + "/TotalOrdering/";
-			
-//			if (System.getSecurityManager() == null) {
-//				System.setSecurityManager(new RMISecurityManager());
-//			}
-			
-			System.out.println("Bind...");
-			
-			String uri = "error";
-			while (true) {
-				try {
-					uri = uri_base + id;
-					Naming.bind(uri, new TotalOrdering());
-				} catch (AlreadyBoundException e) {
-					id++;
-					continue;
-				} catch (ConnectException e) {
-					System.out.println("Failed to connect to registry.");
-					System.exit(0);
-				} catch (ServerException e) {
-					System.out.println("Registry could not find your classes.");
-					e.printStackTrace();
-					System.exit(0);
-				}
-				break;
-			}
-			
-			Runtime.getRuntime().addShutdownHook(new Unbinder(uri));
-			
-			System.out.println("Bound on: " + uri);
-			
-			System.out.println("Connecting to friends...");
-			while (friends.size() != n) {
-				try {
-					ITotalOrdering remote = (ITotalOrdering) Naming.lookup(uri_base + friends.size());
-					friends.add(remote);
-				} catch (java.rmi.NotBoundException e) {
-					Thread.sleep(500);
-				}
-			}
-			
-			System.out.println("Broadcast...");
-			for (int i = 0; i < friends.size(); i++) {
-				ITotalOrdering remote = friends.get(i);
-				try {
-					remote.test("from " + id + ": Hi!");
-				} catch (ConnectException e) {
-					try {
-						remote = (ITotalOrdering) Naming.lookup(uri_base + i);
-						friends.set(i, remote);
-						remote.test("from " + id + ": Hi!");
-					} catch (NotBoundException e2) {
-						System.out.println("Could not connect to remote instance.");
-					} catch (ConnectException e2) {
-						System.out.println("Could not connect to remote instance.");
-					}
-				}
-			}
-		
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(0);
-		}
-		
-		System.out.println("END");			
-	}
-}
-
-class Unbinder extends Thread {
-	public String uri;
-	
-	public Unbinder(String uri) {
-		this.uri = uri;
-	}
-	
-	public void run() {
-		System.out.println("Unbinding uri");
-		try {
-        	Naming.unbind(uri);
-        } catch (Exception e) {
-			e.printStackTrace();
-			System.exit(0);
-		}
-    }
 }
